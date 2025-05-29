5fc2bb2ed80035a8b77f535eda3ceefef99c8b45
Quadeatschool <quadehafen@byui.edu>
2025-05-29 09:10:00 -0600

    update

---
+++ ocean/+energy.cs
@@ -0,0 +1,16 @@
+
+// public class energy
+// {
+
+// public int speed_of_lonlyness;   
+// public int Outgoing;
+// public int Energized_by_social_interaction;
+// public int comfortable_with_attention;
+// public int Excitement_seeking;
+
+// public int CalcEnergyScore(int )
+
+// }
+
+	
+	
---
+++ ocean/cursed.cs
@@ -0,0 +1,21 @@
+// public class curse 
+// {
+//     public int Anxious;
+//     public int Hostile;
+//     public int Emotionally_unstable;
+//     public int Self_conscious;
+//     public int Vulnerable;
+//     public int moody;
+    
+
+    
+
+//     public void CurseEnergy()
+//     {
+
+//     }
+// }
+
+
+
+
